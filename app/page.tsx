3c0887ffe8090438219b65f6ffa852090e9558cf
DevIshola1 <isholad150@gmail.com>
2024-10-04 19:23:15 +0100

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+    <Head>
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <title>ETH NODE</title>
+    </Head>
+
+    <Header />
+
+  </div>
+  );
+}
